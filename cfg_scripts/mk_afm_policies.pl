6fb1e1c22484f40233a28e38831c2e5baf741f3a
Jesse Driskill <jesse.driskill@gmail.com>
2017-02-03 12:22:33 -0800

    Updated taillogs(); removed sv() and se()
    Updated:
    taillogs(): Updated GLOBIGNORE to exclude additional (pointless) files that clutter the screen when you execute the command
    
    Removed:
    sv() and se() - both are no longer required with the proper implementation of chk_vi_mode()

--- cfg_scripts/mk_afm_policies.pl
+++ cfg_scripts/mk_afm_policies.pl
@@ -5,20 +5,22 @@ use strict;
 use Data::Dumper;
 
 my $numPolicy = 1;
-my $numRules  = 700;
+my $numRules  = 250;
 #my $numRules  = 20000;
 #my $numPolicy = 1;
 
-my $aSrcNet  = 0;
-my $bSrcNet  = 0;
-my $cSrcNet  = 0;
-my $dSrcNet  = 1;
+my $aSrcNet   = 0;
+my $bSrcNet   = 0;
+my $cSrcNet   = 0;
+my $dSrcNet   = 1;
+my $srcVlans  = "v107";
 
 my $aDstNet   = 10;
-my $bDstNet   = 111;
+my $bDstNet   = 108;
 my $cDstNet   = 0;
 my $dDstNet   = 0;
 my $dstPorts  = "80";
+my $dstVlans  = "v108";
 
 my $perNetMax = 1000;
 
@@ -34,7 +36,7 @@ for ( my $count = 1; $count <= $numPolicy; $count++ ) {
 
   for ( $rule = 1; $rule <= $numRules; $rule++ ) {
     #printf("    r%03d { action reject ip-protocol tcp destination { port { %d { }}}}\n", $rule, $port);
-    printf("    r%03d { action reject ip-protocol tcp destination { addresses { 10.111.0.0/16 { }} ports { %d { }}} source { vlans { v114 }} }\n", $rule, $port);
+    printf("    r%03d { action reject ip-protocol tcp destination { addresses { 10.%d.0.0/16 { }} ports { %d { }}} source { vlans { %s }} }\n", $rule, $bDstNet, $port, $srcVlans);
     $port++;
   }
 
@@ -48,4 +50,4 @@ for ( my $count = 1; $count <= $numPolicy; $count++ ) {
   #$dDstNet++;
 }
   #printf("    r%03d { action accept ip-protocol tcp destination { port { %s { }}}}\n  }\n}\n", $rule, $dstPorts);
-  printf("    r%03d { action accept ip-protocol tcp destination { addresses { 10.111.0.0/16 { }} ports { %s { }}} source { vlans { v114 }}}\n  }\n}\n", $rule, $dstPorts);
+  printf("    r%03d { action accept ip-protocol tcp destination { addresses { 10.%d.0.0/16 { }} ports { %s { }}} source { vlans { %s }}}\n  }\n}\n", $rule, $bDstNet, $dstPorts, $srcVlans);
