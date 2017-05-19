b69b62cc64099c1eb575ff5c4d021c8132259aef
Jesse Driskill <jesse.driskill@gmail.com>
2017-05-19 08:51:04 -0700

    Minor changes to DUT mgmt aliases and mk_prompt()
    DUT_mgmt.bash:  Added ve01, ve02
    mk_prompt():    Added additional HA states to pattern matching

--- DUT_mgmt.bash
+++ DUT_mgmt.bash
@@ -4,10 +4,6 @@
 ### Seattle Performance test
 ###
 
-## BS4318
-alias bs4318-ldap='ssh jessed@bs4318-ldap'
-alias client-bs4318='ssh jessed@client-bs4318'
-
 ##VE/VMs
 alias ve01='ssh root@ve01'
 alias ve02='ssh root@ve02'
